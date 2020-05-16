ca2166f0a0875dc39bdcf144ba2956a6fa4595d1
tesanti <tesanti@gmail.com>
2020-05-16 18:50:16 -0500

    Removed unecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side JS file has loaded')
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input')
 const message1 = document.querySelector('#message-1')
